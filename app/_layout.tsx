4ff44e4b08bb28fab5ab80c7ca01ee2d76ce7660
Seoyeon HEO <best3487@snu.ac.kr>
2026-08-07 20:45:43 +0900

    feat: implement HomeScreen and SignupScreen for '/' and '/signup'

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,27 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
 import { AuthProvider } from '@/contexts/AuthProvider';
 import { queryClient } from '@/lib/queryClient';
 
 SplashScreen.preventAutoHideAsync();
 
-export default function TabLayout() {
+export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
           <AnimatedSplashOverlay />
-          <AppTabs />
+          <Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="signup" />
+            <Stack.Screen name="calendar" />
+            <Stack.Screen name="explore" />
+          </Stack>
         </ThemeProvider>
       </AuthProvider>
     </QueryClientProvider>
