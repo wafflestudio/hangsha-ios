2828a42debcb589e6291dfcf05a35ecf56c35423
myori <alinwinskingsleigh@snu.ac.kr>
2026-08-13 07:28:11 +0900

    chore: 삭제된 explore 화면의 라우트 등록 제거 - explore 화면 삭제(0e166e4)에서 누락된 _layout.tsx의 Stack.Screen 등록 정리

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout() {
                   <Stack.Screen name="onboarding/interests" />
                   <Stack.Screen name="onboarding/complete" />
                   <Stack.Screen name="calendar" />
-                  <Stack.Screen name="explore" />
                   <Stack.Screen name="mypage" />
                 </Stack>
               </ThemeProvider>
